0b82d26fb9500414d4b57c0d3b7bf91b991f4587
Kazuaki Tanaka <kazuaki@mse.kyutech.ac.jp>
2014-01-30 06:14:33 +0900

    Add database interface

--- server/server.rb
+++ server/server.rb
@@ -6,11 +6,59 @@ require 'gserver'
 
 $manager = 'localhost:9292'
 
+$database = {}
+
+
+# Request GET
+# GET KEY=xxx
+def get_request(req)
+  m = /GET\s+KEY=(?<key>\S+)\s*/i.match(req)
+  return $database[ m[:key] ]
+end
+
+# Request SET
+# SET KEY=xxx VALUE=xxx
+def set_request(req)
+  m = /SET\s+KEY=(?<key>\S+)\s+VALUE=(?<value>\S+)\s*/i.match(req)
+  $database[ m[:key] ] = m[:value]
+  return 'ok'
+end
+
+# Request STATUS
+# STATUS
+def status_request(req)
+  return `uptime`
+end
+
+# Undefined Request
+def undef_request(req)
+  return '*Unknown'
+end
+
+
+
 
 # Database Server
 class Dsmart < GServer
-  def serve(sock)
-    sock.puts('ok')
+  def serve(io)
+    begin
+      req = io.gets
+      
+      if req =~ /^GET\s/i then
+        ret = get_request(req)
+      elsif req =~ /^SET\s/i then
+        ret = set_request(req)
+      elsif req =~ /^STATUS\s/i then
+       ret = status_request(req)
+      else
+        ret = undef_request(req)
+      end
+      
+    rescue
+      io.puts '*Internal Error'
+      return
+    end
+    io.puts ret
   end
 end
 
@@ -31,4 +79,6 @@ uri = URI.parse("http://#{$manager}/add_server?table_name=#{$table_name}&url=#{m
 response, body = Net::HTTP.get(uri)
 
 $db_server.start
-# $db_server.join
+$db_server.join
+
+
